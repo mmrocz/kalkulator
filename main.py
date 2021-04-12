f5035eb4ed405ec05279ca66671f7033c423b8fe
Michał Mroczkowski <mchlmroczkowski@gmail.com>
2021-04-12 23:11:16 +0200

    def function

---
+++ main.py
@@ -0,0 +1,10 @@
+import sys
+
+def kalkulator(rodzaj_działania):
+    print("Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie:" )
+    rodzaj_działania = input()
+    print("podaj pierwszą liczbę")
+    sklanik_1 = input()
+    print("Podaj druga liczbe")
+    skladnik_2 = input()
+
