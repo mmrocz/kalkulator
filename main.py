5d0debf24baf0b5581572f2a9c10f62fed2631ff
Michał Mroczkowski <mchlmroczkowski@gmail.com>
2021-04-12 23:38:40 +0200

    body function

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 import sys
 
-def kalkulator(rodzaj_działania):
-    print("Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie:" )
-    rodzaj_działania = input()
-    print("podaj pierwszą liczbę")
-    sklanik_1 = input()
-    print("Podaj druga liczbe")
-    skladnik_2 = input()
-
+def kalkulator():
+    rodzaj_działania = int(input("Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie: " ))
+    skladnik_1 = int(input("podaj pierwszą liczbę "))
+    skladnik_2 = int(input("Podaj druga liczbe "))
+    if rodzaj_działania == 1:
+        wynik = skladnik_2 + skladnik_1
+        print("Dodaję %s i %s" % (skladnik_1, skladnik_2))
+    elif rodzaj_działania == 2:
+        wynik = skladnik_2 - skladnik_1
+        print("Odejmuję %s i %s" % (skladnik_1, skladnik_2))
+    elif rodzaj_działania == 3:
+        wynik = skladnik_2 * skladnik_1
+        print("Mnożę %s i %s" % (skladnik_1, skladnik_2))
+    elif rodzaj_działania == 4:
+        wynik = skladnik_2 / skladnik_1
+        print("Dzielę %s i %s" % (skladnik_1, skladnik_2))
+    else:
+        exit(1)
+    print("Oto wynik działania %s" % (wynik))
+kalkulator()
